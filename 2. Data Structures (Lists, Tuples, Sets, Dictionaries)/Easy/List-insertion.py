bb104c5c6071caf59af0c9d140ae2e09637669c3
Deep-art-commits <dilpreet.singh12@outlook.com>
2024-06-13 12:18:03 +0530

    13/06/24

---
+++ 2. Data Structures (Lists, Tuples, Sets, Dictionaries)/Easy/List-insertion.py
@@ -0,0 +1,14 @@
+# 2.	Write a program to add an element to a list.
+# Append Method
+lst=[1,2,3,4]
+print(f"Original list :{lst}")
+lst.append([5,'DS'])
+lst.append(6)
+lst.append('singh')
+print(f"Appended  list :{lst}")
+
+import os
+import time
+time.sleep(5)
+os.system('cls')
+
---
+++ 2. Data Structures (Lists, Tuples, Sets, Dictionaries)/Easy/list_creation.py
@@ -0,0 +1,13 @@
+# 1.	How do you create a list in Python?
+# Blank List 
+blank_list=[]
+print(blank_list)
+# static list 
+st_lst=[1,2,3,4,5]
+print(st_lst)
+# list with multiple Datatypes
+listt=[1,2,'Dilpreet']
+print(listt)
+# userdefined list
+ud_list=[i for i in input("Enter List(Comma Seperated: )").split(",")]
+print(f"User_defined_list:{ud_list}")
